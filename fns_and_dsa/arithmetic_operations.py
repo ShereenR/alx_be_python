f0fc98b1096f2130cbd2ef1592e0cc296a0dbbd7
sheren reda <sheryreda557@gmail.com>
2024-10-01 08:32:45 +0300

    create arithmetic_operations.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+def perform_operation():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    if operation =="add":
+        return num1 + num2
+    elif operation =="subtract":
+        return num1 - num2
+    elif operation =="multiply":
+        return num1 * num2
+    elif operation =="divide":
+        if num2 == 0:
+            return "Error: Divistion by zero invalid"
+        return num1 / num2
+    else:
+        return "Eerror: Invalid operation "
+    
+print(perform_operation())
