ab9e1785901edeef33e5fcf184db2744853b378a
Shereen Reda <sheryreda557@gmail.com>
2024-10-14 21:55:17 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,9 +1,11 @@
 class BankAccount:
     def __init__(self,account_balance=0):
         self.__account_balance =account_balance
-    def deposit( self, amount):
+    def deposit(self, amount):
+
+    if amount > 0:
         amount=int(input("Enter your amount you want to deposit: "))
-        self.__account_balance+= amount
+        self.__account_balance += amount
         print(f"Deposited: ${amount}")
     def withdraw(self):
         amount=float(input("Enter your amount you want to withdraw: "))
