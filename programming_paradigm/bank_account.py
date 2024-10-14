af007787defc7932faff72ca1e1a7b3a7b11defe
Shereen Reda <sheryreda557@gmail.com>
2024-10-14 22:48:54 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,15 +1,13 @@
 class BankAccount:
     def __init__(self, account_balance=0):
         self.__account_balance = account_balance
-    def deposit(self):
-        amount = int(input("Enter your amount you want to deposit: "))
+    def deposit(self, amount):
         if amount > 0:
             self.__account_balance += amount
             print(f"Deposited: ${amount}")
         else:
             print("Deposit amount must be positive.")
-    def withdraw(self):
-        amount = float(input("Enter your amount you want to withdraw: "))
+    def withdraw(self, amount):
         if 0 < amount <= self.__account_balance:
             self.__account_balance -= amount
             print(f"Withdrew: ${amount}")
@@ -20,5 +18,5 @@ class BankAccount:
     def display_balance(self):
         print(f"Current Balance: ${self.__account_balance}")
 account = BankAccount()
-account.deposit() 
+account.deposit(67) 
 account.display_balance() 
