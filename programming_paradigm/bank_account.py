b33beca2ff0cb8f7d5b3f539fb1329be8800e46f
sheren reda <sheryreda557@gmail.com>
2024-10-14 21:30:07 +0300

    file one

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self,account_balance=0):
+        self.__account_balance =account_balance
+    def deposit( self):
+        amount=int(input("Enter your amount you want to deposit: "))
+        self.__account_balance+= amount
+        print(f"Deposited:${self.__account_balance}")
+    def withdraw(self):
+        amount=float(input("Enter your amount you want to withdraw: "))
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew:${self.__account_balance}")
+            return True
+        else:
+            return False
+    def display_balance(self):
+        print(f"Current Balance: ${self.__account_balance}")
+
